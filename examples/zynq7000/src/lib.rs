0e3c76d55c769d82d87874bb01eed99a59652056
dadada <dadada@dadada.li>
2023-02-07 16:04:40 +0100

    reintroduce logger

--- examples/zynq7000/src/lib.rs
+++ examples/zynq7000/src/lib.rs
@@ -6,12 +6,17 @@
 use apex_rs::prelude::ApexTimeP1Ext;
 use apex_rs_xng::apex::XngHypervisor;
 use log::{Level, LevelFilter, Metadata, Record};
-use network_partition_xng::network::UartSerial;
+use network_partition_xng::{network::UartSerial, logging::XalLogger};
+use log;
 
 include!(concat!(env!("OUT_DIR"), "/np.rs"));
+
+static LOGGER: XalLogger = XalLogger;
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn main() {
+    unsafe { log::set_logger_racy(&LOGGER).unwrap() };
+    log::set_max_level(log::Level::Trace);
     NetworkPartition.run();
 }
--- xng/src/lib.rs
+++ xng/src/lib.rs
@@ -2,3 +2,40 @@
 #![feature(array_chunks)]
 
 pub mod network;
+
+extern "C" {
+    pub fn XalPrintf(fmt: *const u8, ...) -> i32;
+}
+
+pub mod logging {
+    use XalPrintf;
+
+    pub struct XalLogger;
+
+    impl log::Log for XalLogger {
+        fn enabled(&self, metadata: &Metadata) -> bool {
+            metadata.level() < log::max_level()
+        }
+
+        fn log(&self, record: &Record) {
+            unsafe { XalPrintf(b"AAAAAAAAA\n\0".as_ptr()) };
+
+            // if self.enabled(record.metadata()) {
+            // let mut buf = [0u8; 100];
+            // let level = record.level();
+            //if let Some(args) = record.args().as_str() {
+            //let mut i = 0usize;
+            // for (b, a) in buf[..98].iter_mut().zip(args.as_bytes()) {
+            //     *b = *a;
+            //     i += 1;
+            // }
+            //buf[i] = 0x0;
+            //unsafe { XalPrintf(b"%s\n\0".as_ptr(), buf.as_ptr()) };
+            //}
+            //unsafe { XalPrintf(b"[%s] - [%s]\n\0".as_ptr(), level.as_str().char_indices };
+            // }
+        }
+
+        fn flush(&self) {}
+    }
+}

843827df7b1d182b4ef0f9b5d7293ae03b303e59
autumnal <sven@autumnal.de>
2023-02-09 14:27:31 +0100

    log enhancement

--- xng/src/lib.rs
+++ xng/src/lib.rs
@@ -1,17 +1,18 @@
 #![no_std]
 #![feature(array_chunks)]
+#![feature(let_chains)]
 
 pub mod network;
 
 extern "C" {
-    pub fn XalPrintf(fmt: *const u8, ...) -> i32;
+    // pub fn XalPrintf(fmt: *const u8, ...) -> i32;
+    pub fn XalPutchar(c: i32) -> i32;
 }
 
 pub mod logging {
-    use core::{cmp::min, fmt::Display};
+    use core::fmt::{Display, Write};
 
-    use crate::XalPrintf;
-    use heapless::String;
+    use crate::XalPutchar;
     use log::{LevelFilter, Log, Metadata, Record};
 
     pub struct XalLogger;
@@ -22,44 +23,45 @@ pub mod logging {
         }
 
         fn log(&self, record: &Record) {
-            let mut outstream = String::<200>::new();
-            if record.level() <= LevelFilter::Error
-                && record.file().is_some()
-                && record.line().is_some()
+            let mut writer = XalWriter;
+            if let (lvl, Some(file), Some(line)) =
+                (record.level(), record.file(), record.line()) && lvl <= LevelFilter::Error
             {
-                _ = core::fmt::write(
-                    &mut outstream,
-                    format_args!(
-                        "{}: {} {}: {} at line {}",
-                        record.target(),
-                        ColourLogLevel::from(record.level()),
-                        record.args(),
-                        record.file().unwrap(),
-                        record.line().unwrap(),
-                    ),
+                _ = core::write!(
+                    &mut writer,
+                    "{}: {} {}: {file} at line {line}\n",
+                    record.target(),
+                    ColourLogLevel::from(lvl),
+                    record.args(),
                 );
             } else {
-                _ = core::fmt::write(
-                    &mut outstream,
-                    format_args!(
-                        "{}: {} {}",
-                        record.target(),
-                        ColourLogLevel::from(record.level()),
-                        record.args(),
-                    ),
+                _ = core::write!(
+                    &mut writer,
+                    "{}: {} {}\n",
+                    record.target(),
+                    ColourLogLevel::from(record.level()),
+                    record.args(),
                 );
             }
-
-            let outstream = outstream.as_bytes();
-            let mut buf = [0u8; 200];
-            let len = min(buf.len() - 2, outstream.len());
-            buf[0..len].copy_from_slice(outstream);
-            let end = b"\n\0";
-            buf[len..len + 2].copy_from_slice(end);
-            unsafe { XalPrintf(buf.as_ptr()) };
         }
 
         fn flush(&self) {}
+    }
+
+    pub struct XalWriter;
+
+    impl Write for XalWriter {
+        fn write_str(&mut self, s: &str) -> core::fmt::Result {
+            for c in s.chars() {
+                unsafe { XalPutchar(c as i32) };
+            }
+            Ok(())
+        }
+
+        fn write_char(&mut self, c: char) -> core::fmt::Result {
+            unsafe { XalPutchar(c as i32) };
+            Ok(())
+        }
     }
 
     pub struct ColourLogLevel(log::Level);

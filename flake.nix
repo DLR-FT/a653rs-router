bf22699d082a2c4a4760605131dc4504a7a59feb
Schubert <dadada dadada@dadada.li>
2023-02-09 12:35:01 +0100

    updat apex-rs for error code decoding fix

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
           latest.clippy
           latest.rustfmt
 
-latest.rust-src
+          latest.rust-src
           latest.rust-analyzer
           targets.x86_64-unknown-linux-musl.latest.rust-std
           targets.thumbv7m-none-eabi.latest.rust-std

fce1886add1f42ea2ab85a78880778738b58684c
dadada <dadada dadada@dadada.li>
2023-03-08 13:31:05 +0100

    fix early exit of echo server
    Probably because access to GPIO memory region was not allowed.

--- examples/np-zynq7000/src/lib.rs
+++ examples/np-zynq7000/src/lib.rs
@@ -16,9 +16,9 @@ static TRACER: GpioTracer = GpioTracer::new();
 #[no_mangle]
 pub extern "C" fn main() {
     // The logger should be disabled during measurements
-    //unsafe { log::set_logger_racy(&LOGGER).unwrap() };
+    unsafe { log::set_logger_racy(&LOGGER).unwrap() };
+    log::set_max_level(log::LevelFilter::Trace);
     TRACER.init();
     unsafe { small_trace::set_tracer(&TRACER) }
-    log::set_max_level(log::LevelFilter::Trace);
     NetworkPartition.run();
 }

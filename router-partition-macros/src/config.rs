e2cdf936fc622267242b3773e16feded6a84310c
Tim Schubert <tim.schubert@dlr.de>
2023-10-12 12:45:29 +0200

    router-partition-macros: Add check for duplicate apex names

--- router-partition-macros/src/config.rs
+++ router-partition-macros/src/config.rs
@@ -1,7 +1,9 @@
-use darling::FromMeta;
+use darling::{Error, FromMeta};
+use itertools::Itertools;
 use wrapped_types::{WrappedByteSize, WrappedDuration};
 
 #[derive(Debug, Clone, FromMeta)]
+#[darling(and_then = "Self::verify")]
 pub struct StaticRouterConfig {
     pub hypervisor: Hypervisor,
     pub inputs: usize,
@@ -15,6 +17,32 @@ pub struct StaticRouterConfig {
 
     #[darling(multiple, rename = "port")]
     pub ports: Vec<Port>,
+}
+
+impl StaticRouterConfig {
+    fn verify(self) -> darling::Result<Self> {
+        self.verify_no_duplicate_interfaces_names()?
+            .verify_no_duplicate_port_names()
+    }
+
+    fn verify_no_duplicate_interfaces_names(self) -> darling::Result<Self> {
+        let names: Vec<_> = self.interfaces.iter().map(|i| i.name.clone()).collect();
+        self.verify_no_duplicate_names(&names)
+    }
+
+    fn verify_no_duplicate_port_names(self) -> darling::Result<Self> {
+        let names: Vec<_> = self.ports.iter().map(|p| p.name().clone()).collect();
+        self.verify_no_duplicate_names(&names)
+    }
+
+    fn verify_no_duplicate_names(self, names: &[String]) -> darling::Result<Self> {
+        let mut acc = Error::accumulator();
+        names.iter().duplicates().for_each(|name| {
+            acc.push(Error::duplicate_field(&format!("Duplicate name {}", name)));
+        });
+        acc.finish()?;
+        Ok(self)
+    }
 }
 
 type Hypervisor = syn::Path;
@@ -75,4 +103,72 @@ pub enum Port {
         msg_size: WrappedByteSize,
         msg_count: String,
     },
+}
+
+impl Port {
+    fn name(&self) -> &String {
+        match self {
+            Port::SamplingIn { name, .. } => name,
+            Port::SamplingOut { name, .. } => name,
+            Port::QueuingIn { name, .. } => name,
+            Port::QueuingOut { name, .. } => name,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn duplicate_interface_names() {
+        let cfg = syn::parse_str(
+            r#"
+            router_partition(
+              hypervisor = foo::bar,
+              interface(
+                name = "Dup",
+                kind = foo::bar,
+                destination = "127.0.0.1:51234",
+                mtu = "1KB",
+                rate = "100MB",
+                source = "127.0.0.1:54234"
+              ),
+              interface(
+                name = "Dup",
+                kind = foo::bar,
+                destination = "127.0.0.1:51234",
+                mtu = "1KB",
+                rate = "100MB",
+                source = "127.0.0.1:54234"
+              ),
+              inputs = 2, outputs = 2, mtu = "1.5KB",
+              stack_size = "50MB",
+              time_capacity = "5ms"
+            )
+            "#,
+        )
+        .unwrap();
+        let cfg = StaticRouterConfig::from_meta(&cfg);
+        assert!(cfg.is_err())
+    }
+
+    #[test]
+    fn duplicate_port_names() {
+        let cfg = syn::parse_str(
+            r#"
+            router_partition(
+              hypervisor = foo::bar,
+              port(queuing_in(name = "CAS", discipline = "FIFO", msg_size = "1KB", msg_count = "10")),
+              port(queuing_out(name = "CAS", discipline = "FIFO", msg_size = "1KB", msg_count = "10")),
+              inputs = 2, outputs = 2, mtu = "1.5KB",
+              stack_size = "50MB",
+              time_capacity = "5ms"
+            )
+            "#,
+        )
+        .unwrap();
+        let cfg = StaticRouterConfig::from_meta(&cfg);
+        assert!(cfg.is_err())
+    }
 }

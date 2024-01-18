4279896780d2904b1705a33659774bf713c8bee6
Tim Schubert <dadada@dadada.li>
2024-01-18 15:01:19 +0100

    fix(deps): update a653rs-linux

--- flake.nix
+++ flake.nix
@@ -17,8 +17,7 @@
     };
 
     hypervisor = {
-      #url = "github:DLR-FT/a653rs-linux";
-      url = "github:dadada/apex-linux?ref=udp-network-driver";
+      url = "github:DLR-FT/a653rs-linux";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.devshell.follows = "devshell";
       inputs.fenix.follows = "fenix";
@@ -47,7 +46,7 @@
       cargoLock = {
         lockFile = ./Cargo.lock;
         outputHashes = {
-          "a653rs-linux-0.2.0" = "sha256-r7VzFSs+5Or2zclJD8gFlvCSDwqk8qutHvxbqyNhSPw=";
+          "a653rs-linux-0.2.0" = "sha256-K3fWsAooXrl/uOYRcjI2N3bonSyjc3oeeBBUTJ1X/0M=";
           "a653rs-postcard-0.2.0" = "sha256-xDM5PwV24ZQ3NPVl12A1zX7FvYgLUxcufMCft+BzOSU=";
           "a653rs-xng-0.1.0" = "sha256-7vZ8eWwLXzR4Fb4UCA2GyI8HRnKVR5NFcWumrzkUMNM=";
           "xng-rs-log-0.1.0" = "sha256-YIFFnjWsk6g9tQuRBqmPaXsY3s2+BpkAg5PCw2ZGYCU=";
